5f075cb1ca575a6ddc1afb058f5665eb80374a2c
iamjalenmarshall <115030009+iamjalenmarshall@users.noreply.github.com>
2022-11-05 00:44:36 -0400

    UNFINISHED. Added Arrays and imports
    I added arrays and imports that I think would cover the necessary information needed however I think there is some things needed in
    the code that I was unsure about.

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,10 +1,37 @@
 import "./Styles.css";
-import { currUser } from "../state.js";
+import { useState } from "react";
+import { users} from "../state";
+import { useNavigate } from "react-router-dom";
+import { update } from "../state";
+import Alert from "./Alert";
 
-function Account() {
+
+export default function Account() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+  const [show, setShow] = useState(false);
+
+  //Currently unsure of the proper arrays needed to fill our needed fields 
+  console.log(users);
+
+const navigate = useNavigate();
+
+function showAlert() {
+  setShow(true);
+}
+function hideAlert() {
+  setShow(false);
+}
+// This is another part I am unsure about. I looked in the Login page and if we need 
+//Values I can assign those can create forms as well.
   return (
-   <body>
+    <body>
         <form>
+          
                 <div class="form-wrapper">
                     <label for="floatingUser">Username</label>
                     <input
@@ -36,14 +63,7 @@ function Account() {
                   </div>
                   <button type="submit" class="form-wrapper">Update</button>
         </form>
-
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
-      crossorigin="anonymous"
-    ></script>
     </body>
   );
 }
 
-export default Account;
