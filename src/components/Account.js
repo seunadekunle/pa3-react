2fa71f613c121b587f956448363fdd3cf14b8697
iamjalenmarshall <115030009+iamjalenmarshall@users.noreply.github.com>
2022-11-05 20:15:19 -0400

    Working Account Page

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,9 +1,8 @@
 import "./Styles.css";
 import { useState } from "react";
-import { users} from "../state";
+import { users } from "../state";
 import { useNavigate } from "react-router-dom";
-import { update } from "../state";
-import { currUser } from "../state";
+import { update, currUser } from "../state";
 import Alert from "./Alert";
 
 
@@ -17,78 +16,117 @@ export default function Account() {
   //Currently unsure of the proper arrays needed to fill our needed fields 
   console.log(users);
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-function showAlert() {
-  setShow(true);
-}
-function hideAlert() {
-  setShow(false);
-}
+  function showAlert() {
+    setShow(true);
+  }
+  function hideAlert() {
+    setShow(false);
+  }
 
-function onSubmit(e){
-  e.preventDefault();
+  function onSubmit(e) {
+    e.preventDefault();
 
- update.currUser();
+    if (username.length === 0) {
+      setAlertMessage("You can't leave the Username field empty");
+      showAlert();
+      return;
+    }
 
-}
-// This is another part I am unsure about. I looked in the Login page and if we need 
-//Values I can assign those can create forms as well.
+    if (email.length === 0) {
+      setAlertMessage("You can't leave the Email field empty");
+      showAlert();
+      return;
+    }
+    
+    if (phone.length === 0) {
+      setAlertMessage("You can't leave the Phone field empty");
+      showAlert();
+      return;
+    }
+
+    if((username === currUser.username) && (email === currUser.email) && (phone === currUser.phone)){
+      setAlertMessage("Nothing to update please make a change");
+      showAlert();
+      console.log(username);
+      console.log(email);
+      console.log(phone); 
+      console.log(currUser.username);
+      console.log(currUser.email);
+      console.log(currUser.phone);
+      return;
+      
+
+    }
+
+    update(
+      username,
+      email,
+      phone
+    );
+  }
+  // This is another part I am unsure about. I looked in the Login page and if we need 
+  //Values I can assign those can create forms as well.
   return (
     <body>
-        <form onSubmit={(e) => onSubmit(e)}>
-          
-                <div class="form-wrapper">
-                    <label for="floatingUser">Username</label>
-                    <input
-                      type="username"
-                      class="form-control"
-                      id="floatingUser"
-                      placeholder={currUser.username}
-                      value={username}
-                      onChange={(e) => {
-                      setUsername(e.target.value);
-                      hideAlert();
-                      }}
-                    />
-                  </div>
+      <form onSubmit={(e) => onSubmit(e)}>
 
-                <div class="form-wrapper">
-                    <label for="floatingEmail">Email</label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="floatingEmail"
-                      placeholder={currUser.email}
-                      value={email}
-                      onChange={(e) => {
-                      setEmail(e.target.value);
-                      hideAlert();
-                      }}
-                    />
-                  </div>
+        <div class="form-wrapper">
+          <label for="floatingUser">Username</label>
+          <input
+            type="username"
+            class="form-control"
+            id="floatingUser"
+            placeholder={currUser.username}
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+              hideAlert();
+            }}
+          />
+        </div>
 
-                  <div class="form-wrapper">
-                    <label for="floatingPhone">Phone</label>
-                    <input
-                      type="phone number"
-                      class="form-control"
-                      id="floatingPhone"
-                      placeholder={currUser.telephone}
-                      value={phone}
-                      onChange={(e) => {
-                      setPhone(e.target.value);
-                      hideAlert();
-                      }}
-                    />
-                  </div>
+        <div class="form-wrapper">
+          <label for="floatingEmail">Email</label>
+          <input
+            type="email"
+            class="form-control"
+            id="floatingEmail"
+            placeholder={currUser.email}
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              hideAlert();
+            }}
+          />
+        </div>
 
-                  <div class = "form-wrapper">
-                  <button type="submit" class="form-wrapper">Update</button>
-                  
-                  </div>
-                  
-        </form>
+        <div class="form-wrapper">
+          <label for="floatingPhone">Phone</label>
+          <input
+            type="phone number"
+            class="form-control"
+            id="floatingPhone"
+            placeholder={currUser.telephone}
+            value={phone}
+            onChange={(e) => {
+              setPhone(e.target.value);
+              hideAlert();
+            }}
+          />
+        </div>
+
+        <div class="form-wrapper">
+          <button type="submit" class="btn btn-primary">Update</button>
+
+        </div>
+        <div class = "form-wrapper">
+        {show && <Alert message={alertMessage} />}
+        </div>
+      </form>
+     
     </body>
   );
 }
+
